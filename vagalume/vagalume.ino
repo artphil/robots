b4cc42e8b31f4dba8782b9262dd4b86434eea418
artphil <artphil.dev@gmail.com>
2018-04-16 19:11:37 -0300

    iniciando tp2 - vagalume

--- vagalume/vagalume.ino
+++ vagalume/vagalume.ino
@@ -6,31 +6,6 @@ using namespace std;
 
 /* classes */
 
-class entrada
-{
-	int tam = 50;
-	int i;
-	int valor[50][2];
-public:
-	void set_tam(int n);
-	int  get_tam();
-	void reset();
-	void set(int n, int m);
-	int  get_v1(int n);
-	int  get_v2(int n);
-};
-void entrada::set_tam(int n)
-{ tam = n; }
-int entrada::get_tam()
-{ return tam; }
-void entrada::reset()
-{ i = 0; }
-void entrada::set(int n, int m)
-{	valor[i][0] = n;	valor[i][1] = m;	i++; }
-int entrada::get_v1(int n)
-{ return valor[n][0]; }
-int entrada::get_v2(int n)
-{ return valor[n][1]; }
 
 /* variaveis globais */
 
@@ -40,10 +15,10 @@ LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 // Objeto Motor
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 
-Adafruit_DCMotor *motor_base = AFMS.getMotor(3);
-Adafruit_DCMotor *motor_braco = AFMS.getMotor(4);
+Adafruit_DCMotor *motor_m1 = AFMS.getMotor(1);
+Adafruit_DCMotor *motor_m2 = AFMS.getMotor(3);
 
-#define NOME_ROBO 	" MEGAZORD  v1.0 "
+#define NOME_ROBO 	" MEGAZORD  v2.0 "
 
 #define DIREITA 	1
 #define ESQUERDA 	2
@@ -51,48 +26,47 @@ Adafruit_DCMotor *motor_braco = AFMS.getMotor(4);
 #define BAIXO 		4
 #define SELECIONA 	5
 
+#define RED 		0
+#define GREEN	 	1
+#define BLUE 		2
+
 #define T_MAX_MENU 	180
 
 int 	estado_menu;	// Posicao na pilha do menu
 int 	estado_liga;	// Posicao na pilha do menu 11
 int 	estado_motor;	// Posicao na pilha de acoes do motor
-int 	tdt;			// Tempo de Trabalho (unidade de tempo para cada acao do robo)
-int 	pot_motor_base;	// Forca de Trabalho (aplicada no motor)
-int 	pot_motor_braco;// Forca de Trabalho (aplicada no motor)
-int 	txt;			// Texto a ser escrito;
+int 	pot_motor_m1;	// Forca de Trabalho (aplicada no motor)
+int 	pot_motor_m2;	// Forca de Trabalho (aplicada no motor)
+// int 	txt;			// Texto a ser escrito;
 bool	ligado;			// Informa se o robo esta funcionando;
+float 	tdt;			// Tempo de Trabalho (unidade de tempo para cada acao do robo)
 float 	carga;			// Tensão real da fonte
+float 	cores[3];		// Taxa de reflexao de cor (Red Green Blue)
 unsigned long tempo;	// Contador do tempo corrente
 unsigned long t_menu;	// Contador de tempo do menu
-unsigned long t_liga;	// Contador de tempo do menu 11
+// unsigned long t_liga;	// Contador de tempo do menu 11
 unsigned long t_motor;	// Contador de tempo do motor
 
-/* sequencia de movimentos doa Motores */
-
-entrada ufmg;
-entrada ufmg2;
-entrada teste;
-entrada miss;
-entrada coracao;
 
 /* lista de funcoes utilizadas */
 
 void 	inicia();
 // Inicializa as vatiaveis
-void 	cria_entradas();
-// Inicializa as entradas
 void 	atualiza ();
 // Atualiza as constantes utilizadas
 int 	verifica_botao ();
 // Identifica qual botao foi acionado
 void 	menu();
 // Gerenciador do menu e suas opcoes
-void 	aciona_motor (int m1, int m2);
+void 	aciona_motor (int, int);
 // Contola o acionamento dos motores
-int 	escritor ();
-// Gerencia os movimentos da escrita
-int 	escreve (entrada a);
-// Desenha o vetor
+void 	aciona_luz(bool, bool, bool);
+// Controla o acionamento das luzes
+void 	ve_objeto();
+// Percebe um obstaculo
+void 	ve_cor();
+// Percebe uma cor
+
 
 /* funcoes utilizadas pelo arduino */
 
@@ -107,10 +81,6 @@ void loop()
 	// put your main code here, to run repeatedly:
 	atualiza();
 	menu();
-	if (ligado)
-	{
-		escritor();
-	}
 }
 
 /* implementação das funcoes */
@@ -119,120 +89,22 @@ void inicia() // Inicializa as vatiaveis
 {
 	// Parametros iniciais
 	estado_menu = 0;
-	txt = 0;
 	t_menu = 0;
-	pot_motor_base = 200;
-	pot_motor_braco = 50;
-	tdt = 1500;
+	pot_motor_m1 = 100;
+	pot_motor_m2 = 100;
+	tdt = 10000;
 	ligado = false;
+	cores[RED]	= 0.0;
+	cores[GREEN]= 0.0;
+	cores[BLUE]	= 0.0;
 	lcd.begin(16, 2);
 	AFMS.begin(); // create with the default frequency 1.6KHz
-	init_entradas ();
-}
-
-void init_entradas ()
-{
-		ufmg.set_tam(44);
-		ufmg.reset();
-        // U
-		ufmg.set( 0, 1 );		ufmg.set( 0, 1 );		ufmg.set( 0, 1 );
-		ufmg.set( 1, 0 );		ufmg.set( 1, 0 );		ufmg.set( 0,-1 );
-		ufmg.set( 0,-1 );		ufmg.set( 0,-1 );
-        //
-		ufmg.set( 1, 0 );
-        // F
-		ufmg.set( 0, 1 );		ufmg.set( 0, 1 );		ufmg.set( 0, 1 );
-		ufmg.set( 0,-1 );		ufmg.set( 0,-1 );		ufmg.set( 1, 0 );
-		ufmg.set(-1, 0 );		ufmg.set( 0,-1 );		ufmg.set( 1, 0 );
-		ufmg.set( 1, 0 );
-        // M
-		ufmg.set( 0, 1 );		ufmg.set( 0, 1 );		ufmg.set( 0, 1 );
-		ufmg.set( 0,-1 );		ufmg.set( 0,-1 );		ufmg.set( 0,-1 );
-		ufmg.set( 1, 1 );		ufmg.set( 1,-1 );		ufmg.set( 0, 1 );
-		ufmg.set( 0, 1 );		ufmg.set( 0, 1 );
-        //
-		ufmg.set( 1, 0 );
-        // G
-		ufmg.set( 1, 0 );		ufmg.set( 1, 0 );		ufmg.set( 0,-1 );
-		ufmg.set(-1, 0 );		ufmg.set( 1, 0 );		ufmg.set( 0, 1 );
-		ufmg.set(-1, 0 );		ufmg.set(-1, 0 );		ufmg.set( 0,-1 );
-		ufmg.set( 0,-1 );		ufmg.set( 0,-1 );		ufmg.set( 1, 0 );
-		ufmg.set( 1, 0 );
-
-		teste.set_tam(4);
-		teste.reset();
-		teste.set( 0, 1 );		teste.set( 1, 0 );		teste.set( 0,-1 );
-		teste.set(-1, 0 );   	teste.set( 1, 1 );		teste.set(-1,-1 );
-
-		miss.set_tam(40);
-		miss.reset();
-		// M
-		miss.set( 0, 1 );		miss.set( 0, 1 );		miss.set( 0, 1 );
-		miss.set( 0,-1 );		miss.set( 0,-1 );		miss.set( 0,-1 );
-		miss.set( 1, 1 ); 		miss.set( 1,-1 );      	miss.set( 0, 1 );
-		miss.set( 0, 1 );		miss.set( 0, 1 );
-        //
-		miss.set( 1, 0 );
-        // I
-		miss.set( 0,-1 );		miss.set( 0,-1 );		miss.set( 0,-1 );
-		miss.set( 0, 1 );		miss.set( 0, 1 );		miss.set( 0, 1 );
-		//
-		miss.set( 1, 0 );
-        // S
-		miss.set( 1, 0 );		miss.set( 1, 0 );		miss.set( 0,-1 );
-		miss.set(-1, 0 );		miss.set(-1, 0 );		miss.set( 0,-1 );
-		miss.set( 0,-1 );		miss.set( 1, 0 );		miss.set( 1, 0 );
-        //
-		miss.set( 1, 0 );
-        // S
-		miss.set( 1, 0 );		miss.set( 1, 0 );		miss.set(-1, 0 );
-		miss.set(-1, 0 );		miss.set( 0, 1 );		miss.set( 0, 1 );
-		miss.set( 1, 0 );		miss.set( 1, 0 );		miss.set( 0, 1 );
-		miss.set(-1, 0 );		miss.set(-1, 0 );
-
-		coracao.set_tam(41);
-		coracao.reset();
-		coracao.set( 1, 0 );		coracao.set( 1, 0 );		coracao.set( 1, 0 );
-		coracao.set( 1, 0 );		coracao.set( 0, 1 );		coracao.set( 1, 0 );
-		coracao.set( 0, 1 );		coracao.set( 1, 0 );		coracao.set( 0,-1 );
-		coracao.set( 1, 0 );		coracao.set( 0,-1 );		coracao.set( 1, 0 );
-		coracao.set( 0, 1 );		coracao.set( 1, 0 );		coracao.set( 0, 1 );
-		coracao.set( 1, 0 );		coracao.set( 0, 1 );		coracao.set( 0, 1 );
-		coracao.set(-1, 0 );		coracao.set( 0, 1 );		coracao.set(-1, 0 );
-		coracao.set( 0, 1 );		coracao.set(-1, 0 );		coracao.set( 0, 1 );
-		coracao.set(-1, 0 );		coracao.set( 0, 1 );		coracao.set(-1, 0 );
-		coracao.set( 0,-1 );		coracao.set(-1, 0 );		coracao.set( 0,-1 );
-		coracao.set(-1, 0 );		coracao.set( 0,-1 );		coracao.set(-1, 0 );
-		coracao.set( 0,-1 );		coracao.set(-1, 0 );		coracao.set( 0,-1 );
-		coracao.set( 0,-1 );		coracao.set( 1, 0 );		coracao.set( 0,-1 );
-		coracao.set( 1, 0 );		coracao.set( 0,-1 );
-
-		ufmg2.set_tam(42);
-		ufmg2.reset();
-		// u
-		ufmg2.set( 1, 0 );		ufmg2.set( 0, 1 );		ufmg2.set( 0, 1 );
-		ufmg2.set( 1, 0 );		ufmg2.set( 1, 0 );		ufmg2.set( 0,-1 );
-		ufmg2.set( 0,-1 );		ufmg2.set( 0, 1 );		ufmg2.set( 1, 1 );
-        // f
-		ufmg2.set( 0,-1 );		ufmg2.set( 1,-1 );		ufmg2.set( 1, 0 );
-		ufmg2.set(-1, 0 );		ufmg2.set(-1, 1 );		ufmg2.set( 0, 1 );
-		ufmg2.set( 0, 1 );		ufmg2.set(-1, 1 );		ufmg2.set( 1,-1 );
-		ufmg2.set( 0,-1 );		ufmg2.set( 1, 0 );		ufmg2.set( 1, 0 );
-		// m
-		ufmg2.set( 0,-1 );		ufmg2.set( 1,-1 );		ufmg2.set( 1, 1 );
-		ufmg2.set( 0, 1 );		ufmg2.set( 0,-1 );		ufmg2.set( 1,-1 );
-		ufmg2.set( 1, 1 );		ufmg2.set( 0, 1 );		ufmg2.set( 1, 0 );
-        // g
-		ufmg2.set( 1, 0 );		ufmg2.set( 1, 0 );		ufmg2.set(-1, 0 );
-		ufmg2.set(-1, 0 );		ufmg2.set( 0,-1 );		ufmg2.set( 1,-1 );
-		ufmg2.set( 1, 0 );		ufmg2.set( 0, 1 );		ufmg2.set( 0, 1 );
-		ufmg2.set( 0, 1 );		ufmg2.set(-1, 1 );		ufmg2.set(-1, 0 );
-
 }
 
 void atualiza () // Atualiza as constantes utilizadas
 {
 	carga = 5.0;
+	tempo = millis();
 }
 
 int verifica_botao () // Identifica qual botao foi acionado
@@ -268,7 +140,6 @@ int verifica_botao () // Identifica qual botao foi acionado
 void menu() // Gerenciador do menu e suas opcoes
 {
 	int botao;
-	tempo = millis();
 	if ((tempo - t_menu) > T_MAX_MENU)
 	{
 		t_menu = tempo;
@@ -460,7 +331,7 @@ void menu() // Gerenciador do menu e suas opcoes
 		{
 			lcd.print ("pot m1 -       +");
 			lcd.setCursor(10,1);
-			lcd.print (pot_motor_base);
+			lcd.print (pot_motor_m1);
 
 			if (botao == BAIXO)
 			{
@@ -472,18 +343,18 @@ void menu() // Gerenciador do menu e suas opcoes
 			}
 			else if (botao == DIREITA)
 			{
-				if (pot_motor_base < 255) pot_motor_base++;
+				if (pot_motor_m1 < 255) pot_motor_m1++;
 			}
 			else if (botao == ESQUERDA)
 			{
-				if (pot_motor_base > 0) pot_motor_base--;
+				if (pot_motor_m1 > 0) pot_motor_m1--;
 			}
 		}
 		else if (estado_menu == 23)
 		{
 			lcd.print ("pot m2 -       +");
 			lcd.setCursor(10,1);
-			lcd.print (pot_motor_braco);
+			lcd.print (pot_motor_m2);
 
 			if (botao == BAIXO)
 			{
@@ -495,11 +366,11 @@ void menu() // Gerenciador do menu e suas opcoes
 			}
 			else if (botao == DIREITA)
 			{
-				if (pot_motor_braco < 255) pot_motor_braco++;
+				if (pot_motor_m2 < 255) pot_motor_m2++;
 			}
 			else if (botao == ESQUERDA)
 			{
-				if (pot_motor_braco > 0) pot_motor_braco--;
+				if (pot_motor_m2 > 0) pot_motor_m2--;
 			}
 		}
 		else if (estado_menu == 29)
@@ -543,6 +414,26 @@ void menu() // Gerenciador do menu e suas opcoes
 		}
 		else if (estado_menu == 32)
 		{
+			lcd.print ("R    G    B     ");
+			lcd.setCursor(1,1);
+			lcd.print (cores[RED]);
+			lcd.setCursor(6,1);
+			lcd.print (cores[GREEN]);
+			lcd.setCursor(11,1);
+			lcd.print (cores[BLUE]);
+
+			if (botao == BAIXO)
+			{
+				estado_menu = 33;
+			}
+			else if (botao == CIMA)
+			{
+				estado_menu = 31;
+			}
+
+		}
+		else if (estado_menu == 33)
+		{
 			lcd.print ("Tempo          s");
 			lcd.setCursor(7,1);
 			lcd.print (millis());
@@ -553,7 +444,7 @@ void menu() // Gerenciador do menu e suas opcoes
 			}
 			else if (botao == CIMA)
 			{
-				estado_menu = 31;
+				estado_menu = 32;
 			}
 
 		}
@@ -600,7 +491,7 @@ void menu() // Gerenciador do menu e suas opcoes
 		}
 		else if (estado_menu == 42)
 		{
-			lcd.print ("E  Motor Base  D");
+			lcd.print ("F   Motor  1   T");
 			lcd.setCursor(10,1);
 
 			if (botao == BAIXO)
@@ -628,12 +519,12 @@ void menu() // Gerenciador do menu e suas opcoes
 		}
 		else if (estado_menu == 43)
 		{
-			lcd.print ("C  Motor Braco B");
+			lcd.print ("F   Motor  2   T");
 			lcd.setCursor(10,1);
 
 			if (botao == BAIXO)
 			{
-				estado_menu = 44;
+				estado_menu = 49;
 			}
 			else if (botao == CIMA)
 			{
@@ -654,57 +545,6 @@ void menu() // Gerenciador do menu e suas opcoes
 				aciona_motor(0,0);
 			}
 		}
-		else if (estado_menu == 44)
-		{
-			lcd.print ("      MISS      ");
-
-			if (botao == BAIXO)
-			{
-				estado_menu = 45;
-			}
-			else if (botao == CIMA)
-			{
-				estado_menu = 43;
-			}
-			else if (botao == SELECIONA)
-			{
-				estado_menu = 441;
-			}
-		}
-		else if (estado_menu == 45)
-		{
-			lcd.print ("    CORACAO     ");
-
-			if (botao == BAIXO)
-			{
-				estado_menu = 46;
-			}
-			else if (botao == CIMA)
-			{
-				estado_menu = 44;
-			}
-			else if (botao == SELECIONA)
-			{
-				estado_menu = 451;
-			}
-		}
-		else if (estado_menu == 46)
-		{
-			lcd.print ("      ufmg      ");
-
-			if (botao == BAIXO)
-			{
-				estado_menu = 49;
-			}
-			else if (botao == CIMA)
-			{
-				estado_menu = 45;
-			}
-			else if (botao == SELECIONA)
-			{
-				estado_menu = 461;
-			}
-		}
 		else if (estado_menu == 49)
 		{
 			lcd.print ("Voltar          ");
@@ -715,7 +555,7 @@ void menu() // Gerenciador do menu e suas opcoes
 			}
 			else if (botao == CIMA)
 			{
-				estado_menu = 45;
+				estado_menu = 43;
 			}
 			else if (botao == SELECIONA)
 			{
@@ -806,34 +646,6 @@ void menu() // Gerenciador do menu e suas opcoes
 		{
 			if (!ligado) {
 				ligado 	= true;
-				txt 	= 2;
-				estado_motor = -1;
-			}
-			estado_menu = 0;
-		}
-		else if (estado_menu == 441)
-		{
-			if (!ligado) {
-				ligado 	= true;
-				txt 	= 3;
-				estado_motor = -1;
-			}
-			estado_menu = 0;
-		}
-		else if (estado_menu == 451)
-		{
-			if (!ligado) {
-				ligado 	= true;
-				txt 	= 4;
-				estado_motor = -1;
-			}
-			estado_menu = 0;
-		}
-		else if (estado_menu == 461)
-		{
-			if (!ligado) {
-				ligado 	= true;
-				txt 	= 5;
 				estado_motor = -1;
 			}
 			estado_menu = 0;
@@ -845,77 +657,53 @@ void aciona_motor (int m1, int m2)
 {
 	if (m1 > 0)
 	{
-		motor_base->setSpeed(pot_motor_base);
-		motor_base->run(FORWARD);
+		motor_m1->setSpeed(pot_motor_m1);
+		motor_m1->run(FORWARD);
 	}
 	else if (m1 < 0)
 	{
-		motor_base->setSpeed(pot_motor_base);
-		motor_base->run(BACKWARD);
+		motor_m1->setSpeed(pot_motor_m1);
+		motor_m1->run(BACKWARD);
 	}
 	else
 	{
-		motor_base->run(RELEASE);
+		motor_m1->run(RELEASE);
 	}
 
 	if (m2 > 0)
 	{
-		motor_braco->setSpeed(pot_motor_braco);
-		motor_braco->run(FORWARD);
+		motor_m2->setSpeed(pot_motor_m2);
+		motor_m2->run(FORWARD);
 	}
 	else if (m2 < 0)
 	{
-		motor_braco->setSpeed(pot_motor_braco);
-		motor_braco->run(BACKWARD);
+		motor_m2->setSpeed(pot_motor_m2);
+		motor_m2->run(BACKWARD);
 	}
 	else
 	{
-		motor_braco->run(RELEASE);
+		motor_m2->run(RELEASE);
 	}
 }
 
-int escritor ()
+void aciona_luz(bool r, bool g, bool b)
 {
-	if (txt == 0) return 0;
-	if (txt == 1)
-	{
-		escreve(ufmg);
-	}
-	else if (txt == 2)
-	{
-		escreve(teste);
-	}
-	else if (txt == 3)
-	{
-		escreve(miss);
-	}
-	else if (txt == 4)
-	{
-		escreve(coracao);
-	}
-	else if (txt == 5)
-	{
-		escreve(ufmg2);
-	}
+	if (r) ;
+	else ;
+
+	if (g) ;
+	else ;
+
+	if (b) ;
+	else ;
 }
 
-int escreve (entrada a)
+void ve_objeto()
 {
-	tempo = millis();
-	if ((tempo - t_motor) > tdt)
-	{
-		t_motor = tempo;
-		estado_motor++;
-	}
 
-	if (estado_motor < a.get_tam()) {
-		aciona_motor(a.get_v1(estado_motor),a.get_v2(estado_motor));
-	}
-	else
-	{
-		ligado 	= false;
-		txt     = 0;
-		aciona_motor(0,0);
-	}
-	return 0;
+}
+
+void ve_cor()
+{
+
 }
